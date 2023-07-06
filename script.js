8f69774d8449895d53bcd6d89b57d074e3918a42
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-07-06 18:36:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 //your code here!
+//your code here!
+let totalList=10;
+let OlList=document.getElementById('infi-list');
 
+window.addEventListener('load',()=>{
+    for(let i=1;i<=totalList;i++){
+        let li=document.createElement('li');
+        li.innerText=`Item ${i}`;
+        OlList.append(li);
+        }
+});
+
+
+OlList.addEventListener('scroll',()=>{
+    // console.log('hello');
+    // console.log(OlList.scrollTop);
+   // let top=Math.floor(OlList.scrollTop);
+//   console.log(top);
+//    console.log(OlList.scrollHeight);
+let height=OlList.offsetHeight;
+let offset=OlList.scrollTop;
+
+if(offset+height>=OlList.scrollHeight){
+    console.log('at bottom');
+
+    for(let i=1;i<=2;i++){
+        let li=document.createElement('li');
+        li.innerText=`Item ${++totalList}`;
+        OlList.append(li);
+        }
+}
+})
